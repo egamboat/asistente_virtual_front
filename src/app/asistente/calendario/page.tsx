6778d90f41816fe0423eec70d8b7342fea3a22c4
egamboat <egamboat@unemi.edu.ec>
2024-12-03 23:58:55 -0500

    fix error calendar

--- src/app/asistente/calendario/page.tsx
+++ src/app/asistente/calendario/page.tsx
@@ -2,6 +2,9 @@
 
 import Reloj from "@/components/reloj/reloj";
 import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { EventApi } from '@fullcalendar/core';
+
 import esLocale from "@fullcalendar/core/locales/es";
 import { FC, useEffect, useState } from "react";
 import { Evento } from "@/interfaces/interfaceEventos";
@@ -9,6 +12,13 @@ import { customFetch } from "@/components/refresh_token";
 
 const Calendario: FC = () => {
   const [dataEvent, setEvents] = useState<any[]>([]);
+  const [calendarId, setCalendarId] = useState<string>("");
+  const [accessToken, setAccessToken] = useState<string>("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token") || "";
+    setAccessToken(token);
+  }, []); // Solo se ejecuta en el cliente
 
   // Transformar eventos del backend para FullCalendar
   const transformEvents = (events: Evento[]) => {
@@ -52,6 +62,21 @@ const Calendario: FC = () => {
     fetchEvents();
   }, []);
 
+  // Obtener calendarId desde localStorage
+  useEffect(() => {
+    const userData = localStorage.getItem("userData");
+    if (userData) {
+      const parsedUserData = JSON.parse(userData);
+      setCalendarId(parsedUserData.email);
+    } else {
+      console.error("No hay un ID de calendario");
+    }
+  }, []);
+
+
+
+  // const googleCalendarApiKey = `${process.env.GOOGLE_CALENDAR_API_KEY}`;
+
   return (
     <div className="h-full bg-white flex flex-col justify-between">
       <div className="flex justify-between w-full mt-4">
@@ -66,17 +91,14 @@ const Calendario: FC = () => {
       <div className="flex items-center justify-center rounded-lg pb-2 w-full">
         <div className="bg-gray-100 rounded-lg p-2 w-full mx-w-xl md:max-w-2xl">
           <FullCalendar
+            plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             weekends={true}
             locale={esLocale}
             selectable={true}
-            // Configuración de múltiples fuentes de eventos
-            eventSources={[
-              {
-                events: dataEvent, // Eventos del backend
-              },
-            ]}
+            events={dataEvent}
           />
+
         </div>
       </div>
     </div>
